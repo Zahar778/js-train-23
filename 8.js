4ca2e94f7bed279678e5a717e3507a43eaf85b3d
Zahar778 <zax.kobra@gmail.com>
2023-11-20 19:49:43 +0200

    work

--- 8.js
+++ 8.js
@@ -3,6 +3,15 @@
 
 // Клас User відповідає за користувача, який може відправляти повідомлення.
 class User {
+  constructor(name, messenger) {
+    this.name = name;
+    this.messenger = messenger;
+  }
+  sendMessage(message){
+    console.log(`${this.name} відправив повідомлення ${message}`)
+    return message;
+  }
+
   // Створюємо конструктор класу, який приймає name та messenger як параметри та ініціалізує їх
   // Робимо метод sendMessage який відправляє повідомлення за допомогою відповідного месенджера, та виводить в консоль `${this.name} відправив повідомлення ${message}`.
   // Він приймає один параметр - message - повідомлення, яке потрібно відправити за допомогою методу sendMessage.
@@ -11,11 +20,19 @@ class User {
 
 // Клас SMSMessenger відповідає за відправку повідомлень за допомогою SMS.
 class SMSMessenger {
+  sendMessage(message) {
+    console.log(`Відправлено SMS: ${message}`)
+    return message;
+  }
   // Створюємо статичний метод sendMessage який приймає один параметр - message, та виводить в консоль `Відправлено SMS: ${message}`
 }
 
 // Клас EmailMessenger відповідає за відправку повідомлень за допомогою Email.
 class EmailMessenger {
+  sendMessage(message) {
+    console.log(`Відправлено Email: ${message}`)
+    return message;
+  }
   // Створюємо статичний метод sendMessage який приймає один параметр - message, та виводить в консоль `Відправлено Email: ${message}`
 }
 
@@ -23,11 +40,11 @@ console.log("Завдання 7 ====================================");
 // Після виконання розкоментуйте код нижче
 
 // Створюємо двох користувачів - John та Jane - які відправляють повідомлення за допомогою різних месенджерів.
-// const john = new User("John", SMSMessenger);
-// const jane = new User("Jane", EmailMessenger);
+const john = new User("John", SMSMessenger);
+const jane = new User("Jane", EmailMessenger);
 
 // John відправляє повідомлення за допомогою SMS.
-// john.sendMessage("Привіт!"); // Виведе: Відправлено SMS: [John]: Привіт!
+john.sendMessage("Привіт!"); // Виведе: Відправлено SMS: [John]: Привіт!
 
 // Jane відправляє повідомлення за допомогою Email.
-// jane.sendMessage("Привіт!"); // Виведе: Відправлено Email: [Jane]: Привіт!
+jane.sendMessage("Привіт!"); // Виведе: Відправлено Email: [Jane]: Привіт!
